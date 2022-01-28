64336270efb51757fdcdfd17dd1d5101a462df53
leticiaandrade <leticia933@gmail.com>
2022-01-27 22:43:45 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import Header from './components/Header';
 
 class App extends React.Component {
   render() {
-    return (<div>
-      <Header />
-    </div>);
+    return (
+      <div>
+        <Header />
+      </div>);
   }
 }
 
